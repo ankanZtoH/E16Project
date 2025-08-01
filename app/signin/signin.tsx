d0124c4ca8069ce0620714450ae0e72138ab5d42
ankanZtoH <ankan06edu@gmail.com>
2025-08-01 21:14:29 +0530

    file update

--- app/signin/signin.tsx
+++ app/signin/signin.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const signin = () => {
   return (
-    <Button className='font-bold border-1 border-black bg-white text-black'>SingIn</Button>
+    <Button className='font-bold border-2 border-black bg-white text-black hover:bg-[#ea763f] hover:border-none hover:text-white'>SingIn</Button>
   )
 }
 
--- app/signup/signup.tsx
+++ app/signup/signup.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const signup = () => {
   return (
     <div>
-      <Button variant="secondary" className='font-bold bg-blue-700'>SignUp</Button>
+      <Button variant="secondary" className='font-bold bg-[#ea763f] hover:border-2 hover:border-black dark:hover:bg-white dark:hover:text-black'>SignUp</Button>
     </div>
   )
 }
