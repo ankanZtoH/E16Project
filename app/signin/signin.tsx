5eae5cccca4b6bf0e81499f1a2c13ff5c833ae4b
ankanZtoH <ankan06edu@gmail.com>
2025-08-02 14:02:42 +0530

    file updated

--- app/signin/signin.tsx
+++ app/signin/signin.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const signin = () => {
   return (
-    <Button className='cursor-pointer font-bold border-2 border-black bg-white text-black hover:bg-[#ea763f] hover:border-orange-500 hover:text-white hover:scale-110 hover:duration-400 hover:ease-in-out'>SingIn</Button>
+    <Button className='cursor-pointer font-bold border-2 border-black bg-white text-black hover:bg-[#ea763f] hover:border-orange-500 hover:text-white hover:duration-400 hover:ease-in-out'>SingIn</Button>
   )
 }
 
